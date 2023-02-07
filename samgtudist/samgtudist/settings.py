deaa53faf15d1e63dd037b830bd70ce86d2d2907
Дмитрий Фёдоров <dmitrijfedorov@MacBook-Air-Dmitrij.local>
2023-02-07 16:18:14 +0300

    Вынесены перменные окружения в config.json

--- samgtudist/samgtudist/settings.py
+++ samgtudist/samgtudist/settings.py
@@ -1,10 +1,14 @@
 import os
 from datetime import timedelta
 from pathlib import Path
+import json
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+
+with open("config.json") as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
